866aea3b5311924894e7e68a0c688fbdf6d584c3
Bhavesh Damor <bhaveshdamor5555@gmail.com>
2022-07-08 19:39:43 +0530

    final

--- server/index.js
+++ server/index.js
@@ -45,10 +45,14 @@ app.use(errorMiddleware);
 
 const port = process.env.PORT || 4000;
 
-app.use(express.static(path.join(__dirname , '../client/build')));
+// app.use(express.static(path.join(__dirname , '../client/build')));
 
-app.get('*' , (req,res) => {
-    res.sendFile(path.resolve(__dirname , '../client/build/index.html'))
+// app.get('*' , (req,res) => {
+//     res.sendFile(path.resolve(__dirname , '../client/build/index.html'))
+// })
+
+app.get('/',(req,res)=>{
+  res.status(200).json({message:"Circuit flare backend"})
 })
 
 app.listen(port, () => {
